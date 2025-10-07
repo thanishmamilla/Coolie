5b411f32b1df500755ac1894efa240942bdf74ee
thanishmamilla <129726162+thanishmamilla@users.noreply.github.com>
2025-10-07 12:44:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     // Keep CORS for API testing during development
-    origin: "https://dynamic-hotteok-ea464c.netlify.app",
+    origin: {"https://dynamic-hotteok-ea464c.netlify.app","http://localhost:5173"},
     credentials: true
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
